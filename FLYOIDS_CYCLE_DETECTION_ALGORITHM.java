e50876d8f5697df21bf1dd3bcc0612159dd6b3c6
Kastro .R <kastrouchiha@gmail.com>
2025-11-26 18:32:23 +0530

    Refactor cycle detection algorithm structure

--- FLYOIDS_CYCLE_DETECTION_ALGORITHM.java
+++ FLYOIDS_CYCLE_DETECTION_ALGORITHM.java
@@ -14,7 +14,7 @@ public class FLYOIDS_CYCLE_DETECTION_ALGORITHM {
 // */
 //public class Solution {
 //    public boolean hasCycle(ListNode head) {
-//        ListNode slow = head,fast = head;
+//        ListNode slow = head,fast = head;       // use two pointers(TORTOISE , RABBIT) one who jumps one node and another one jumps two .if it meets at the same point the linked list is in rounded loop not contain end
 //        while(fast != null && fast.next != null){
 //            slow = slow.next;
 //            fast = fast.next.next;
@@ -40,7 +40,7 @@ public class FLYOIDS_CYCLE_DETECTION_ALGORITHM {
 //public class Solution {
 //    public ListNode detectCycle(ListNode head) {
 //        ListNode slow = head,fast = head;
-//        while(slow != null && fast != null && fast.next != null){
+//        while(slow != null && fast != null && fast.next != null){   // used to find which node is the starting point of the rounded loop
 //            slow = slow.next;
 //            fast = fast.next.next;
 //            if(slow == fast){
@@ -55,5 +55,6 @@ public class FLYOIDS_CYCLE_DETECTION_ALGORITHM {
 //        return null;
 //    }
 //}
+
 
 
